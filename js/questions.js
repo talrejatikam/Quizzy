f9e629f1ea59c4d6f5a033cb15a92973bb387e74
talrejatikam <tikamtalreja@outlook.com>
2021-03-09 15:57:00 +0530

    step1 basic layout

---
+++ js/questions.js
@@ -0,0 +1,79 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is CEO of Persistent systems? does HTML stand for?",
+    answer: "Sandeep kalra",
+    options: [
+      "Anand deshpande",
+      "Sandeep kalra",
+      "Abhay jere",
+      "None of the above"
+    ]
+  },
+    {
+    numb: 2,
+    question: "When Persistent Systems Launches Specialized Engineering Services for IBM Watson IoT Platform?",
+    answer: "2016",
+    options: [
+      "2015",
+      "2016",
+      "2017",
+      "2018"
+    ]
+  },
+    {
+    numb: 3,
+    question: "When persistent systems was founded?",
+    answer: "16 May 1990",
+    options: [
+      "16 March 1990",
+      "6 May 1990",
+      "6 March 1991",
+      "16 May 1990"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is a unique software platform that helps you understand and optimise your building's environment, maintenance, usage and health?",
+    answer: "Paxonix ",
+    options: [
+      "NovaQuest",
+      "PRM cloud ",
+      "Paxonix ",
+      "Metrikus"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Is microsoft a partner of Persistent systems?",
+    answer: "TRUE",
+    options: [
+      "TRUE",
+      "FALSE"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Who was Pulse 2021 winner ?",
+    answer: "FARSIGHTED YELLOW",
+    options: [
+      "RESPONSIBLE GREEN",
+      "CURIOUS ORANGE",
+      "FARSIGHTED YELLOW",
+      "INGENIOUS BLUE"
+    ]
+  },
+  {
+    numb: 7,
+    question: "which of following is not the policy of persistent systems??",
+    answer: "Restrictive Clauses in Employment Contracts",
+    options: [
+      "Ethics Policy",
+      "Anti-corruption Policy",
+      "Code of Conduct Policy",
+      "Restrictive Clauses in Employment Contracts"
+    ]
+  },
+];
